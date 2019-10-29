59a378f0abbbdad180e289a90c1373b3b35825c1
alexeispirit <alexeispirit@gmail.com>
2019-10-29 11:42:45 +0500

    14-iterator

---
+++ 14-iterator/iteratorObject.js
@@ -0,0 +1,27 @@
+class Iterator {
+  constructor(el) {
+    this.index = 0;
+    this.keys = Object.keys(el);
+    this.elements = el;
+  }
+
+  next() {
+    return this.elements[this.keys[this.index++]];
+  }
+
+  hasNext() {
+    return this.index < this.keys.length;
+  }
+}
+
+const autos = {
+  audi: { model: "Audi", color: "black", price: 20000 },
+  bmw: { model: "BMW", color: "white", price: 30000 },
+  tesla: { model: "Tesla", color: "gray", price: 40000 }
+};
+
+const collection = new Iterator(autos);
+
+while (collection.hasNext()) {
+  console.log(collection.next());
+}
